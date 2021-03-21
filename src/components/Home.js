f615fb747e52332dd6379ffc183070c6e6614ea9
Ricardo <francoliricardo@gmail.com>
2021-03-21 18:32:45 +0100

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+	return (
+		<div>
+			<h1>Start making good decisions!</h1>
+			<button>Start</button>
+		</div>
+	);
+};
+
+export default Home;
---
+++ src/questions.js
@@ -0,0 +1,32 @@
+const questions = [
+	{
+		id: 1,
+		question: 'Question1',
+		imageUrl: 'www.photorul.com',
+		answers: [
+			{ answer: 'Answer 1', value: 1 },
+			{ answer: 'Answer 2', value: 2 },
+			{ answer: 'Answer 3', value: 3 },
+		],
+	},
+	{
+		id: 2,
+		question: 'Question2',
+		imageUrl: 'www.photorul.com',
+		answers: [
+			{ answer: 'Answer 1', value: 1 },
+			{ answer: 'Answer 2', value: 2 },
+		],
+	},
+	{
+		id: 3,
+		question: 'Question3',
+		imageUrl: 'www.photorul.com',
+		answers: [
+			{ answer: 'Answer 1', value: 1 },
+			{ answer: 'Answer 2', value: 2 },
+		],
+	},
+];
+
+export default questions;
