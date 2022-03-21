a7787ce72f26370f266b3a54f2f2f3cf48eac6e5
Ricardo Francoli <francoliricardo@gmail.com>
2022-03-21 17:24:54 +0100

    check this out

--- src/components/Outcome.js
+++ src/components/Outcome.js
@@ -2,30 +2,28 @@ import React from 'react';
 import { Button, Grid, Typography } from '@material-ui/core';
 
 const Outcome = ({ outcome, finishQuiz }) => {
-	return (
-		<Grid
-			container
-			direction={'column'}
-			spacing={0}
-			justify={'center'}
-			alignContent={'center'}
-			alignItems={'center'}
-			style={{ minHeight: '100vh' }}
-		>
-			<Typography variant='h2'>{outcome.title}</Typography>
-			<Grid item xs={6} sm={4} md={3} style={{ margin: '30px 0' }}>
-				<Typography variant='body1'>{outcome.body}</Typography>
-			</Grid>
-			<Button
-				onClick={finishQuiz}
-				variant='contained'
-				size='large'
-				style={{ backgroundColor: '#cf3476', color: '#fcfcfc' }}
-			>
-				Go back to Home
-			</Button>
-		</Grid>
-	);
+  return (
+    <Grid
+      container
+      direction={'column'}
+      spacing={0}
+      justify={'center'}
+      alignContent={'center'}
+      alignItems={'center'}
+      style={{ minHeight: '100vh' }}>
+      <Typography variant='h2'>{outcome.title}</Typography>
+      <Grid item xs={6} sm={4} md={3} style={{ margin: '30px 0' }}>
+        <Typography variant='body1'>{outcome.body}</Typography>
+      </Grid>
+      <Button
+        onClick={finishQuiz}
+        variant='contained'
+        size='large'
+        style={{ backgroundColor: '#cf3476', color: '#fcfcfc' }}>
+        Home
+      </Button>
+    </Grid>
+  );
 };
 
 export default Outcome;
